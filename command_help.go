7936fbaf4a6cec19c22d1d4b5f6f03b1da17b44c
Lucas Giraldelli <vinicius.giraldelli@hotmail.com>
2024-02-05 17:36:23 -0400

    feat: basic command structure

---
+++ command_help.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func callbackHelp() error {
+  fmt.Println("Welcome to Pokedex help menu!")
+  fmt.Println("Here are your available commands:")
+
+  availableCommands := getCommands()
+
+  for _, cmd := range availableCommands {
+    fmt.Printf("- %v: %s\n",
+      cmd.name,
+      cmd.description,
+    );
+  }
+
+  fmt.Println("")
+
+  return nil
+}
+
